dbf31e2222e40edcbdbfe9b2c02286186f1f7c4b
Bruceshark <yyubv@connect.ust.hk>
2021-11-18 00:03:21 +0800

    change the default style of components, add styleClass to fxml

--- src/main/java/comp3111/covid/controller/TableController.java
+++ src/main/java/comp3111/covid/controller/TableController.java
@@ -80,6 +80,7 @@ public class TableController {
 
         datePicker.getEditor().setDisable(true);
         datePicker.getEditor().setOpacity(1);
+        datePicker.setValue(utils.dateToLocalDate(fileOperator.getMinimumDate()));
 
         final Callback<DatePicker, DateCell> tableCellFactory =
                 new Callback<DatePicker, DateCell>() {
@@ -94,11 +95,11 @@ public class TableController {
                                         utils.dateToLocalDate(fileOperator.getMinimumDate()))
                                 ) {
                                     setDisable(true);
-                                    setStyle("-fx-background-color: #ffc0cb;");
+                                    getStyleClass().add("disabled-date");
                                 }
                                 if (item.isAfter(utils.dateToLocalDate(fileOperator.getMaximumDate()))) {
                                     setDisable((true));
-                                    setStyle("-fx-background-color: #ffc0cb;");
+                                    getStyleClass().add("disabled-date");
                                 }
                             }
                         };
